b4ff649b250abbd9240f9b097e8de127a0ecff4c
Arjan Verkerk <arjan.verkerk@gmail.com>
2017-06-19 10:50:51 +0200

    Add code (commented) for single precipitation image extraction.

--- raster_feeder/steps/rotate.py
+++ raster_feeder/steps/rotate.py
@@ -38,11 +38,12 @@ def download(current=None):
     """ Dummy downloader for debugging purposes. """
     # return open file object
     path = 'steps_epf60_SydM-nc_20170519_1700.nc'
+    # path = 'steps_precip10_SydM-nc_20170519_0550.nc'
     logger.info('Using dummy file "{}".'.format(path))
     yield path
 
 
-def extract_region_no_warp(path):
+def extract_region(path):
     """
     Return latest steps data as raster store region.
 
@@ -57,15 +58,20 @@ def extract_region_no_warp(path):
         time_units = valid_time.units
         time_data = valid_time[:]
         time = netCDF4.num2date(time_data, units=time_units)
+        # time = [netCDF4.num2date(time_data, units=time_units)]
 
         precipitation = nc.variables['precipitation']
         data = precipitation[0, :, ::-1]
+        # import numpy as np
+        # data = precipitation[::-1][np.newaxis]
         no_data_value = precipitation._FillValue.item()
 
     return regions.Region.from_mem(
         data=data,
+        # data=data.filled(no_data_value),
         time=time,
         bands=(0, config.DEPTH),
+        # bands=(0, 1),
         fillvalue=no_data_value,
         geo_transform=config.NATIVE_GEO_TRANSFORM,
         projection=osr.GetUserInputAsWKT(str(config.NATIVE_PROJECTION))
@@ -79,7 +85,7 @@ def rotate_steps():
     # retrieve updated data
     try:
         with download() as path:
-            region = extract_region_no_warp(path)
+            region = extract_region(path)
     except:
         logger.exception('Error:')
         return
